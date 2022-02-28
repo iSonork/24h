bef1f72735e0ef22ae4bda66eb3e53888a72a1d9
iSonork <82953829+iSonork@users.noreply.github.com>
2022-02-28 19:19:14 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+var mineflayer = require('mineflayer');
+var pass = "12345"; //Authme plugini şifresi (Plugin yoksa silmeyin)
+var ayar = {
+  host: "sunucuip", //Sunucunuzun ipsi
+  port: 25565, //Değiştirme!!!
+  username: "BotBotando", //Botun ismi Kafanıza Göre
+  version: false //Değiştirme!!!
+  
+};
+
+var bot = mineflayer.createBot(ayar);
+
+
+bot.on('chat', function(username, message) {
+  if (username === bot.username) return;
+  function intervalFunc() {
+    bot.setControlState('forward', true)
+  }
+  setInterval(intervalFunc,7000);
+  console.log('Sunucuya Giriş Yapıldı!');
+  //bot.chat('/login '+ pass);
+  bot.chat('Salve familia! BotBotando passando aqui :3');
+});
+
+bindEvents(bot);
+function bindEvents(bot) {
+  
+  bot.on('error', function(err) {
+    console.log("Bir Hata Oluştu!");
+  });
+  
+  bot.on('end', function() {
+    console.log("Bot sunucudan atıldı!");
+    setTimeout(relog, 5000);
+  });
+  
+  function relog() {
+    console.log("Sunucuya tekrardan bağlanılıyor");
+    bot = mineflayer.createBot(ayar);
+    
+    bot.on('chat', function(username, message) {
+      if (username === bot.username) return;
+      console.log("Bot tekrardan sunucuya giriş yaptı!");
+      //bot.chat('/login '+ pass);
+      bot.chat('Salve familia! BotBotando passando aqui :3');
+});
+    
+    
+    bindEvents(bot);
+  }
+}
+
+  
+  
